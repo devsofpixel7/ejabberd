f1673b20cf78a6ccc9ac2738f4a38685f0bff53d
Hervie Hrvoje Kotarac <h.kotarac@gmail.com>
2018-12-01 03:18:04 +0100

    add guzzle request

--- src/Ejabberd.php
+++ src/Ejabberd.php
@@ -2,72 +2,168 @@
 /**
  * ejabberd package file
  * User: hervie
- * Date: 29/11/2018
+ * Date: 14/10/2018
  * Time: 4:02 PM
  */
 
 namespace Ejabberd;
 
-class Ejabberd
+use Ejabberd\Commands\Contracts\IEjabberdCommand;
+use Ejabberd\Commands\CreateUser;
+use Ejabberd\Commands\SendMessage;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use JsonSerializable;
+use Illuminate\Support\Facades\Log;
+
+class Ejabberd implements JsonSerializable
 {
 
+    private $api = '';
+    private $user = '';
+    private $password = '';
+    private $domain = '';
+    private $conference_domain = '';
+    private $debug = '';
 
-    static function usersConnectedNumber()
+
+    public function __construct()
     {
-        return self::CallApi('GET', env('EJABBERD_API').'connected_users_number');
+        $this->api = env('EJABBERD_API');
+        $this->user = env('EJABBERD_USER');
+        $this->password = env('EJABBERD_PASSWORD');
+        $this->domain = env('EJABBERD_DOMAIN');
+        $this->conference_domain =  env('EJABBERD_DOMAIN');
+        $this->debug = env('EJABBERD_DEBUG');
+
     }
 
-    static function usersConnectedInfo()
+    /**
+     * @param IEjabberdCommand $command
+     */
+    public function executeQueue(IEjabberdCommand $command)
     {
-        return self::CallApi('GET', env('EJABBERD_API').'connected_users_info');
+
     }
 
-    static function usersConnected()
+    /**
+     * @param CreateUser $createUser
+     * @return null|\Psr\Http\Message\StreamInterface
+     */
+    public function createUser(CreateUser $createUser)
     {
-        return self::CallApi('GET', env('EJABBERD_API').'connected_users');
+        return $this->execute($createUser);
     }
 
-    // Method: POST, PUT, GET etc
-    // Data: array("param" => "value") ==> index.php?param=value
-
-    static function CallApi($method, $url, $data = false)
+    /**
+     * @param SendMessage $sendMessage
+     * @return null|\Psr\Http\Message\StreamInterface
+     */
+    public function sendMessage(SendMessage $sendMessage)
     {
-        $curl = curl_init();
+        return $this->execute($sendMessage);
+    }
 
-        switch ($method)
-        {
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
 
-                if ($data)
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                break;
-            case "PUT":
-                curl_setopt($curl, CURLOPT_PUT, 1);
-                break;
-            default:
-                if ($data)
-                    $url = sprintf("%s?%s", $url, http_build_query($data));
+    public function usersConnectedNumber()
+    {
+        return self::callApi('GET', 'connected_users_number', '', 'usersConnectedNumber');
+    }
+
+    public function usersConnectedInfo()
+    {
+        return self::callApi('GET', 'connected_users_info', '', 'usersConnectedInfo');
+    }
+
+    public function usersConnected()
+    {
+        return self::callApi( 'GET', 'connected_users', '', 'usersConnected');
+    }
+
+    public function roomsList()
+    {
+        return self::callApi('GET', 'muc_online_rooms?host='.$this->domain, '', 'muc_online_rooms');
+    }
+
+    /**
+     * @param IEjabberdCommand $command
+     * @return null|\Psr\Http\Message\StreamInterface
+     */
+    public function execute(IEjabberdCommand $command)
+    {
+        $client = new Client([
+            'verify' => false,
+            'base_uri' => $this->api
+        ]);
+        $command_name = $command->getCommandName();
+        try {
+            $response = $client->request('POST', $command_name, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                ],
+                'auth' => [
+                    $this->user, $this->password
+                ],
+                'json' => $command->getCommandData()
+            ]);
+            if ($this->debug) {
+                Log::info($command->getCommandName() . 'executed successfully.');
+            }
+            return $response->getBody();
+
+        } catch (GuzzleException $e) {
+            if ($this->debug) {
+                Log::info("Error occurred while executing the command " . $command->getCommandName() . ".");
+            }
+            return null;
+        } catch (\Exception $e) {
+            if ($this->debug) {
+                Log::info("Error occurred while executing the command " . $command->getCommandName() . ".");
+            }
+            return null;
+        }
+    }
+
+    /**
+     * @param IEjabberdCommand $command
+     * @return null|\Psr\Http\Message\StreamInterface
+     */
+    public function callApi($method, $url, $data = false, $command)
+    {
+        $client = new \GuzzleHttp\Client();
+
+        try {
+            $res = $client->request($method, $this->api.$url, [
+                'headers' => [
+                    'Accept'     => 'application/json'
+                ]
+            ]);
+
+            return json_decode($res->getBody(), JSON_PRETTY_PRINT);
+
+        } catch (ClientException $e) {
+            if ($this->debug=='true') {
+
+               Log::info("Error occurred while executing the command " . $command . ".");
+               return json_decode($e->getResponse()->getBody(true), JSON_PRETTY_PRINT);
+
+            }
+            return null;
         }
 
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-        $result = curl_exec($curl);
-
-        if (curl_error($curl)) {
-            $response = curl_error($curl);
-        }
-        else
-        {
-            $response = response()->json(json_decode($result, JSON_UNESCAPED_SLASHES));
-        }
-
-        curl_close($curl);
-
-        return $response;
-
     }
+
+    /**
+     * @param Object
+     * @return null|\JsonSerializable
+     */
+    public function jsonSerialize() {
+        return [
+            'status', 'code' , 'message', 'num_sessions'
+        ];
+    }
+
 
 }
